bda5c6411b87bf9ebc9b2bc1fb1e1d034e0ef7db
Kenichi Go Uang <kenichi.uang@synacy.com>
2017-05-05 10:44:40 +0800

    Create specs for lesson03 exercise
    Signed-off-by: Kenichi Go Uang <kenichi.uang@synacy.com>

---
+++ src/test/groovy/com/synacy/lesson03/exercise/EnrollmentServiceSpec.groovy
@@ -0,0 +1,106 @@
+package com.synacy.lesson03.exercise
+
+import com.synacy.lesson03.exercise.domain.CourseClass
+import com.synacy.lesson03.exercise.domain.EnrollmentDto
+import com.synacy.lesson03.exercise.domain.Printable
+import com.synacy.lesson03.exercise.domain.Student
+import com.synacy.lesson03.exercise.domain.StudentEmailType
+import com.synacy.lesson03.exercise.domain.StudentProfile
+import com.synacy.lesson03.exercise.domain.StudyLoad
+
+/**
+ * Created by kenichigouang on 5/4/17.
+ */
+class EnrollmentServiceSpec extends spock.lang.Specification {
+
+    EnrollmentService service
+
+    StudentProfileService studentProfileService
+    CourseClassService courseClassService
+    EnrollmentNotificationService enrollmentNotificationService
+    StudyLoadFormatter studyLoadFormatter
+    SystemService systemService
+
+    void setup() {
+        service = new EnrollmentService()
+
+        studentProfileService = Mock()
+        courseClassService = Mock()
+        enrollmentNotificationService = Mock()
+        studyLoadFormatter = Mock()
+        systemService = Mock()
+
+        service.studentProfileService = studentProfileService
+        service.courseClassService = courseClassService
+        service.enrollmentNotificationService = enrollmentNotificationService
+        service.studyLoadFormatter = studyLoadFormatter
+        service.systemService = systemService
+    }
+
+    def "ProcessEnrollment should update student profile on processing on enrollment"() {
+        given:
+        EnrollmentDto enrollmentDetails = Mock()
+        StudentProfile studentProfile = Mock()
+
+        enrollmentDetails.getStudentProfile() >> studentProfile
+        enrollmentDetails.getEnrolledClasses() >> []
+
+        when:
+        service.processEnrollment(enrollmentDetails)
+
+        then:
+        1 * studentProfileService.updateStudentProfile(studentProfile)
+    }
+
+    def "ProcessEnrollment should enroll student to a class/classes on processing of enrollment"() {
+        given:
+        EnrollmentDto enrollmentDetails = Mock()
+        Student student = Mock()
+
+        CourseClass courseClass = Mock()
+
+        def enrolledClasses = [
+                courseClass
+        ]
+
+        enrollmentDetails.getStudent() >> student
+        enrollmentDetails.getEnrolledClasses() >> enrolledClasses
+
+        when:
+        service.processEnrollment(enrollmentDetails)
+
+        then:
+        1 * courseClassService.enrollStudentToClass(student, courseClass)
+    }
+
+    def "ProcessEnrollment should notify student via email on processing of enrollment"() {
+        given:
+        EnrollmentDto enrollmentDetails = Mock()
+
+        enrollmentDetails.getEnrolledClasses() >> []
+
+        when:
+        service.processEnrollment(enrollmentDetails)
+
+        then:
+        1 * enrollmentNotificationService.emailStudent(StudentEmailType.ENROLLMENT, enrollmentDetails)
+
+    }
+
+    def "ProcessEnrollment should have the system print study load in study load format"() {
+        given:
+        EnrollmentDto enrollmentDetails = Mock()
+        StudyLoad studyLoad= Mock()
+
+        enrollmentDetails.getEnrolledClasses() >> []
+        studyLoadFormatter.format(enrollmentDetails) >> studyLoad
+        Printable studyLoadPrintable = studyLoad
+
+        when:
+        service.processEnrollment(enrollmentDetails)
+
+        then:
+        1 * systemService.print(studyLoadPrintable)
+
+    }
+}
